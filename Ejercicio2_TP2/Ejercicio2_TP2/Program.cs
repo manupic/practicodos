036df662150de1f505debdde823a0e930af0dca8
ema <ema.pic@hotmail.com.ar>
2024-04-08 20:26:49 -0300

    Ejercicio 2

---
+++ Ejercicio2_TP2/Ejercicio2_TP2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Ejercicio2_TP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingreso_str = Console.ReadLine();
+            int ingreso = Convert.ToInt32(ingreso_str);
+            bool primo = true;            
+
+            for (int i=1; i <= ingreso; i++)
+            {
+                if(i != 1 && i!=ingreso)
+                {
+                    if (ingreso % i == 0)
+                    {
+                        primo = false;
+                        break;
+                    }
+                }                
+            }
+
+            if (primo) 
+            {
+                Console.WriteLine($"El número {ingreso} es primo");
+            }
+            else
+            {
+                Console.WriteLine($"El número {ingreso} NO es primo");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
